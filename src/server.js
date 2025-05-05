ee21c7ae371c06e0469eb99548d4b0baf548e972
Minseo Kim <minseo.dev@gmail.com>
2025-05-05 16:54:32 +0900

    middleware - morgan 사용

--- src/server.js
+++ src/server.js
@@ -1,44 +1,21 @@
 import express from "express";
-
+import morgan from "morgan";
 const PORT = 4000;
 
 const app = express();
-
-
-const privateMiddleware = (req, res , next) => {
-    const url = req.url;
-    if(url === "/protected") {
-        return res.send("<h1>Not Allowed</h1>");
-    }
-    console.log("Allowed, you nay continue.");
-    next();
-}
-
-const handleLogin = (req , res) => {
-    return res.send({message : "Login here"});
-};
-
-const handleProtected = (req, res) => {
-    return res.send("Welcome to the private lounge.");
-}
-
-app.use(privateMiddleware);
-
-const routerLogger = (req, res, next) => {
-    console.log("PATH",req.path);
-    next();
-};
-const methodLogger =(req, res, next)=> {
-    console.log("METHOD",req.method);
-    next();
-};
+const logger = morgan("dev");
 const home = (req, res) =>{
     console.log("I will respond.");
-    res.send("hello");
+    return res.send("hello");
 } 
-app.use(methodLogger, routerLogger);
+
+const login = (req,res) =>{
+    return res.send("login");
+}
+
+app.use(logger);
 app.get("/",  home);
-app.get("/protected",handleProtected);
+app.get("/login",login);
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
 
