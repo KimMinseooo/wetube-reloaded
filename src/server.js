a86b597fdee3b48320c9eb3b6000e21acc1ca7ee
Minseo Kim <minseo.dev@gmail.com>
2025-05-05 16:43:58 +0900

    middleware 복습

--- src/server.js
+++ src/server.js
@@ -4,21 +4,42 @@ const PORT = 4000;
 
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-    console.log(`Someone is going to : ${req.url}`);
+
+const privateMiddleware = (req, res , next) => {
+    const url = req.url;
+    if(url === "/protected") {
+        return res.send("<h1>Not Allowed</h1>");
+    }
+    console.log("Allowed, you nay continue.");
     next();
 }
 
-const handleHome = (req , res) => {
-    return res.send("I love middlewares");
-};
 const handleLogin = (req , res) => {
     return res.send({message : "Login here"});
 };
 
-app.use(gossipMiddleware);
-app.get("/", handleHome);
-app.get("/login",handleLogin);
+const handleProtected = (req, res) => {
+    return res.send("Welcome to the private lounge.");
+}
+
+app.use(privateMiddleware);
+
+const routerLogger = (req, res, next) => {
+    console.log("PATH",req.path);
+    next();
+};
+const methodLogger =(req, res, next)=> {
+    console.log("METHOD",req.method);
+    next();
+};
+const home = (req, res) =>{
+    console.log("I will respond.");
+    res.send("hello");
+} 
+app.use(methodLogger, routerLogger);
+app.get("/",  home);
+app.get("/protected",handleProtected);
+
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
