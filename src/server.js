65ed08dfaacfe319ba0c22b7dd90ccc978f83f4b
Minseo Kim <minseo.dev@gmail.com>
2025-05-08 01:31:56 +0900

    create mongoose & db.js

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+import "./db"; // db파일 자체를 import!!
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
@@ -17,6 +18,6 @@ app.use("/videos", videoRouter);
 app.use("/users",userRouter);
 
 const handleListening = () => 
-    console.log(`Server listening on port http://localhost:${PORT}`);
+    console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
