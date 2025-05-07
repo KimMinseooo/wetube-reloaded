39ccd754cb4ef509d15fad74cf78979b476369ca
Minseo Kim <minseo.dev@gmail.com>
2025-05-08 00:42:35 +0900

    practice get & post

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -26,7 +26,7 @@ let videos = [
 ];
 
 export const trending = (req, res) => {
-    res.render("home", {pageTitle: "Home" , videos});
+    return res.render("home", {pageTitle: "Home" , videos});
 }
 export const watch = (req, res) => {
     const { id } = req.params;
@@ -43,4 +43,23 @@ export const postEdit = (req, res) => {
     const { title } = req.body;
     videos[id-1].title = title;
     return res.redirect(`/videos/${id}`);
+};
+
+export const getUpload = (req, res) => {
+    return res.render("upload", { pageTitle: "Upload Video" });
+};
+
+export const postUpload = (req, res) => {
+    // here we will add a video to the videos array.
+    const { title } = req.body;
+    const newVideo = {
+        title,
+        rating: 0,
+        comments: 0,
+        createdAt: "just now",
+        views: 0,
+        id: videos.length + 1,
+    };
+    videos.push(newVideo);
+    return res.redirect("/");
 }
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,13 @@
 import express from "express";
-import {watch, edit,upload,deleteVideo, getEdit, postEdit} from "../controllers/videoController";
+import {watch, edit,upload,deleteVideo, getEdit, postEdit, getUpload, postUpload} from "../controllers/videoController";
 const videoRouter = express.Router();
 
+videoRouter.route("/upload").get(getUpload).post(postUpload);
+// videoRouter.get("/upload", getUpload);
+// videoRouter.post("/upload",postUpload);
 // videoRouter.get(`/:id(\\d+)`, see);
 videoRouter.get("/:id", watch);
 videoRouter.route("/:id/edit").get(getEdit).post(postEdit);
 // videoRouter.get("/:id/edit", getEdit);
 // videoRouter.post("/:id/edit",postEdit);
-
 export default videoRouter;
